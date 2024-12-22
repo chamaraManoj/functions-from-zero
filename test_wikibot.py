2516a313738735c2f8d872ae26961d0434d5047e
chamaraManoj <chamarammkkcc@gmail.com>
2024-12-22 20:22:07 +0000

    new test functions

--- test_wikibot.py
+++ test_wikibot.py
@@ -1,5 +1,10 @@
 from wikibot import scrap
 
+from click.testing import CliRunner
+
 def test_scrap():
-    assert "Microsoft" in scrap("Microsoft", 10)
+    runner = CliRunner()
+    result = runner.invoke(scrap, ['--name','Microsoft', '--length', '10'])
+    assert result.exit_code == 0
+    assert 'Microsoft' in  result.output
 
--- wikibot.py
+++ wikibot.py
@@ -1,8 +1,14 @@
 import wikipedia
 import click
 
+@click.command()
+@click.option('--name', help = 'Page to search')
+@click.option('--length', help = 'Length of the output')
 def scrap(name="Microsoft", length = 1):
     results = wikipedia.summary(name, sentences=length)
+    click.echo(click.style(f'results: {results}', fg='red' ))
     return results
 
-print(scrap())
+if __name__ =='__main__':
+    scrap()
+
