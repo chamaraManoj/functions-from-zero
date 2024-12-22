26bddfe21694e1de48c681769348ca1b279c81e5
chamaraManoj <chamarammkkcc@gmail.com>
2024-12-22 07:58:20 +0000

    adding files

---
+++ my_statements.py
@@ -0,0 +1,8 @@
+my_lsit= ['apple','cherry','lemon']
+my_dict = {'drink':'coffee', 'milk':'fully'}
+
+for item in my_lsit:
+    print(f"Printing the items {item}")
+
+for key, value in my_dict:
+    print('My favourite to drink {key} and {value}')
