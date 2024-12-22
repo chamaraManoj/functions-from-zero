71b8e88f7cdd5b13d5bd22f4f6dceade85e7576a
chamaraManoj <chamarammkkcc@gmail.com>
2024-12-22 08:22:09 +0000

    added make file

---
+++ Makefile
@@ -0,0 +1,13 @@
+install:
+	pip install --upgrade pip && pip install -r requirements.txt
+
+test:
+	python -m pytest -vv test_hello.py
+
+format:
+	black *.py
+
+lint: 
+	pylint hello.py
+
+all: install  test 
--- my_statements.py
+++ my_statements.py
@@ -1,8 +1,8 @@
-my_lsit= ['apple','cherry','lemon']
-my_dict = {'drink':'coffee', 'milk':'fully'}
+my_lsit = ["apple", "cherry", "lemon"]
+my_dict = {"drink": "coffee", "milk": "fully"}
 
 for item in my_lsit:
     print(f"Printing the items {item}")
 
 for key, value in my_dict:
-    print('My favourite to drink {key} and {value}')
+    print("My favourite to drink {key} and {value}")
